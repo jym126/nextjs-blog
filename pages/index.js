0ca451d88ae0c8c2c0189afaff621f8322a0973e
jym126 <jym126@hotmail.com>
2023-11-20 22:13:50 +0100

    update 2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import .
- from 'next/link';
+import Link from 'next/link';
 import Date from '../components/dates';
 
 
